cfb163d163e286cb6c65437a5d75810d8e0f4d4f
uroccjo <jorge.roccatagliata@pearson.com>
2019-11-29 19:21:46 -0700

    added button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Amplify from 'aws-amplify';
+import Amplify, { API } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import {withAuthenticator} from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 
